d5cbe6218d2d0fcddba70f3e170e1a362d959abd
Younghoon Choi <ozymandias089@gmail.com>
2025-10-28 16:37:49 +0900

    test: Post publish & archived rename guards (RED)

---
+++ src/test/java/com/y11i/springcommddd/unit/posts/PostTest.java
@@ -0,0 +1,15 @@
+package com.y11i.springcommddd.unit.posts;
+
+import org.junit.jupiter.api.Test;
+
+public class PostTest {
+    @Test
+    void givenDraft_whenPublish_thenStatusBecomesPublished() {
+        /* RED */
+    }
+
+    @Test
+    void givenArchive_whenRename_thenThrowArchiveModification() {
+        /* RED */
+    }
+}
