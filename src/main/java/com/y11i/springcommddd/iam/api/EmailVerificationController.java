3f656ea248e14c69d1ed55274ca55f0317a23f75
Younghoon Choi <ozymandias089@gmail.com>
2025-10-29 23:38:34 +0900

    fix: Resolved api path conflict

--- src/main/java/com/y11i/springcommddd/iam/api/EmailVerificationController.java
+++ src/main/java/com/y11i/springcommddd/iam/api/EmailVerificationController.java
@@ -23,14 +23,14 @@ public class EmailVerificationController {
     private final EmailVerificationUseCase emailVerificationUseCase;
 
     // 가입 인증 토큰 재발송
-    @PostMapping("/signup/request")
+    @PostMapping("/signup/request/me")
     public ResponseEntity<Void> requestForSignup(@Valid @RequestBody EmailVerificationSignupRequestDTO requestDTO) {
         // 이메일만으로 재전송을 허용하려면, 이메일→멤버 조회 유스케이스를 써서 memberId를 찾아 호출하면 됨.
         // 하지만 여기선 간단하게 회원이 로그인 상태에서 내 주소로 재발송하는 API를 권장.
         return ResponseEntity.status(501).build();
     }
 
-    @PostMapping("/signup/confirm")
+    @PostMapping("/signup/request")
     public ResponseEntity<Void> requestForSignupMe(@AuthenticatedMember MemberId memberId, @Valid @RequestBody EmailVerificationSignupRequestDTO requestDTO) {
         emailVerificationUseCase.requestForSignup(memberId.id(), requestDTO.getEmail());
         return ResponseEntity.accepted().build();
